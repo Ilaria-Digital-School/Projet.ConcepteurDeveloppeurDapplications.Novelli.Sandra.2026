f8652b3645a2b35b3a2924a885d025f2866c7213
Sandra <sandra.nova.666@gmail.com>
2026-08-02 01:32:56 +0200

    sign-in page

--- A-CUCINA/src/app/components/header/header.ts
+++ A-CUCINA/src/app/components/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
